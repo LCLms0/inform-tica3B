056564a94be4e9828a468c3b87eaa4ab04ca9122
uGabrielS2 <lucaslemoss608@gmail.com>
2025-08-05 10:59:29 -0300

    atividade de 1 ao 4

---
+++ exercicio.py
@@ -0,0 +1,28 @@
+# QUESTÃO 1  
+
+# n = 0
+# for n in range (1,11 ):
+#     print(f"número :  {n}" )
+
+# QUESTÃO 2
+
+# contador = 11
+# while contador  <=  11 :
+#     contador -= 1
+#     print(f"número :  {contador}")
+#     if contador == 0 :
+#         break 
+
+#  QUESTÃO 3
+
+# nome = input('digite seu nome: ')
+# for n in range (1,6) : 
+#     print(f"olá {nome}")
+
+# QUESTÃO 4
+
+# numero=0
+# for n in range (1,11) :
+#     numero += 2
+#     print(f'{numero}')
+#     continue
