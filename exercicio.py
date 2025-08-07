2090d500d2d7352f1f094ea02f99eae17ce1de6a
uGabrielS2 <lucaslemoss608@gmail.com>
2025-08-07 09:09:04 -0300

    questões de 1 a 6

--- exercicio.py
+++ exercicio.py
@@ -26,3 +26,24 @@
 #     numero += 2
 #     print(f'{numero}')
 #     continue
+
+# QUESTÃO 5
+
+# senha= "1234"
+# senha2 = input('digite sua senha: ')
+# while senha != senha2 :
+#     print('seja incorreta.')
+#     senha2 = input('tente novamente: ')
+# while senha == senha :
+#     print("acesso liberado")
+#     break
+
+# QUESTÃO 6
+
+# nu1=float (input("Digite um número: "))
+# nu2=float (input("Digite um número: "))
+# nu3=float (input("Digite um número: "))
+# nu4=float (input("Digite um número: "))
+# nu5=float (input("Digite um número: "))
+# soma= nu1+nu2+nu3+nu4+nu5
+# print(f"a soma é: {soma}")
