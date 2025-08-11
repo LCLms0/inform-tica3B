d7c3f846a4cd291d127c365d67d6bf4a26250dbc
uGabrielS2 <lucaslemoss608@gmail.com>
2025-08-11 11:44:44 -0300

    Questão 1 do exercicio 2

---
+++ exercicio2.py
@@ -0,0 +1,16 @@
+# QUESTÃO 1 
+
+# for i in range (1 , 4 ) :
+#     nota= float(input('Digite sua nota:  ') )
+# media= (nota+nota+nota) / 3 
+# print(f"Sua média é : {media}  ")
+# if media  < 7 :
+#     print("você está reprovado !")
+# else :
+#     print('você foi aprovado !')
+
+# QUESTÃO 2
+
+# nome=input("Digite uma palavra:  ")
+# for nome in (range ( a , A , E , e , I , i , O , o , u , U ) )   :
+#     print(f"suas vogais são: {nome}")
