bf9a54f1f74e6c19eea26510ba133d6cadf0c518
wesley8266 <lucaslemoss608@gmail.com>
2025-08-12 10:44:40 -0300

    questões de 1 a 2

--- exercicio2.py
+++ exercicio2.py
@@ -11,6 +11,12 @@
 
 # QUESTÃO 2
 
-# nome=input("Digite uma palavra:  ")
-# for nome in (range ( a , A , E , e , I , i , O , o , u , U ) )   :
-#     print(f"suas vogais são: {nome}")
+# contador=0
+# vogal = [ 'a' , 'A' , 'E' , 'e' , 'i' , 'I', 'O' , 'o' , 'u' , 'U']
+# n=input("Digite uma palavra:  ")
+# for v in vogal:
+#     if v in n :
+#         print(f'{v}')
+#         contador += 1 
+#         continue
+
