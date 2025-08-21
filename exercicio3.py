4b606b75bfaff610ae2ce037484004187b09208e
uGabrielS2 <lucaslemoss608@gmail.com>
2025-08-21 09:08:15 -0300

    exercicio 3, questões 1 finalizada

---
+++ exercicio3.py
@@ -0,0 +1,26 @@
+# QUESTÃO 1
+
+# peso = float(input("Digite seu Peso corporal (em quilos):  "))
+# altura= float(input("Digite sua altura (em metros):  "))
+# imc = peso /    ( altura * altura ) 
+# if imc < 18.5 :
+#     print(f"Seu Índice de Massa Corporal é {imc} você está : Abaixo do peso")
+# if imc >= 18.5   <=  24.9 :
+#     print(f"Seu Índice de Massa Corporal é {imc} você está : Peso normal")
+# if imc  >=25   <= 29.9 :
+#     print(f"Seu Índice de Massa Corporal é {imc} você está : sobrepeso")
+# if imc > 30  :
+#     print(f"Seu Índice de Massa Corporal é {imc} você está : Obesidade")
+# print("Fim do Programa")
+
+# QUESTÃO 2
+
+contador=0
+n= int(input("Digite um número inteiro para eu lhe informar os números pares e impar até esse número:  "))
+par=  n   %  2 
+impar = n % 1 
+for i in range(n) :
+    print(f"  ")
+    contador += 1
+    
+
