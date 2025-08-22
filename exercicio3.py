7d0b671532b7fda053f14970c8cd706c6d417929
wesley8266 <lucaslemoss608@gmail.com>
2025-08-22 10:20:00 -0300

    questões de 1 a 3

--- exercicio3.py
+++ exercicio3.py
@@ -15,12 +15,27 @@
 
 # QUESTÃO 2
 
-contador=0
-n= int(input("Digite um número inteiro para eu lhe informar os números pares e impar até esse número:  "))
-par=  n   %  2 
-impar = n % 1 
-for i in range(n) :
-    print(f"  ")
-    contador += 1
+# n= int(input("Digite um número inteiro para eu lhe informar os números pares e impar até esse número:  "))
+# for i in range(1,n + 1) :
+#     if i % 2 == 0 :
+#         print(f"{i} é par")
+#     else :
+#         print(f" {i} é impar")
     
+# QUESTÃO 3
 
+# n = int(input("Digite um número inteiro que vou lhe informar a tabuada desde número:  "))    
+# print(f"Tabuada do {n}:")
+# for i in range(1, 11):
+#         print(f"{n} x {i} = {n * i}")
+
+# QUESTÃO 4
+
+n = float(input("Digite um número e lhe direi se ele é primo ou não:  "))
+for i in range(2, int((n))):
+    if n / i == 0 :    
+        print(f"{n} não é Primo")    
+    else:
+        print(f"{n} é Primo!")
+
+    
