b6a2185a11807638feff03e1ba5cdda800b0db5d
uGabrielS2 <antonio.meireles1@aluno.ce.gov.br>
2025-09-04 09:11:14 -0300

    quase concluido

--- exercicio3.py
+++ exercicio3.py
@@ -47,14 +47,41 @@
 # alunosquenãopassaram=0
 # somanotas=0
 # alunos=int(input("digite a quantidade de alunos: "))
-# for aluno in range(0,alunos) :
+# for i in range(0,alunos) :
 #     nota=float(input("digite a nota do aluno: "))
-#     somanotas + nota
+#     somanotas += nota
 #     if nota <= 6 :
-#         alunosquenãopassaram += 1
+#         alunosquepassaram += 1
 #     if nota > 6 :
-#         alunosquepassaram +=1
-# media= somanotas / alunos
+#         alunosquenãopassaram +=1
+# media= somanotas/alunos
 # print(f" media da turma foi: {media}")
 # print(f"a quantidade que passou foi {alunosquepassaram}")
 # print(f"a quantidade que não passou foi {alunosquenãopassaram}")
+
+# QUESTÃO 6
+
+# import random
+# print("BEM VINDO AO JOGO DE ADIVINHAÇÕES!") 
+# print("REGRAS: 1 - TENTE ADIVINHAR O NÚMERO SECRETO")
+# n_aleatorio = 0 
+# pontuação = 0
+# for n in range(1 , 20):
+#     n_aleatorio=int(random.Random())
+#     n_usuario= int(input("Tente adivinhar o número:  "))
+#     if n_usuario < n_aleatorio :
+#         print("quase lá...tente novamente mas com um número menor.")
+#         continue
+#     if n_usuario > n_aleatorio :
+#         print("quase lá...tente novamente mas com um número maior.")
+#         continue
+#     if n_usuario == n_aleatorio :
+#         print("você acertou!!! Ganhou + 100 de pontuação ! (digite 0 para parar)")
+#         pontuação += 100
+#         continue
+#     if n_aleatorio == 0 :
+#         break
+# import time
+# print(f"Sua Pontuação Final foi...")
+# time.sleep(3)
+# print(f"{pontuação}!!!") 
