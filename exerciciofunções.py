9e47f75f0d00fa161e826745607cf9e54d9b6a26
vinicius-araujo777 <lucaslemoss608@gmail.com>
2025-09-09 11:05:43 -0300

    exercicio de funções de 1 a 8

---
+++ exerciciofunções.py
@@ -0,0 +1,90 @@
+# QUESTÃO 1
+
+# nome=input("digite seu nome:  ")
+# def saudação(nome):
+#         return f"Olá, {nome}! Seja bem-vindo ao curso de lógica de programação."
+# print(saudação(nome))
+
+# QUESTÃO 2
+
+# n=int(input("Digite um número: "))
+# def par_impar(n):
+#     if n % 2 == 0:
+#         return f"Esse número é par!"
+#     else :
+#         return f"Esse número é impar!"
+# print(par_impar(n))
+
+# QUESTÃO 3
+
+# def com(a,b):
+#     a=float(input("digite um numero:"))
+#     b=float(input("digite outro numero:"))
+#     if a>b:
+#         return print(f"{a} é maior que {b}")
+#     if a<b:
+#         return print(f"{b} é maior que {a}")
+#     else:
+#         return print(f"{a} e {b} são iguais")
+# com(a=0,b=0)   
+    
+# QUESTÃO 4
+
+# def tabuada(n) :
+#     n = int(input("digite um número e ,eu lhe direi sua tabuada:  "))
+#     for i in range(1, 11):
+#         print(f"{n} x {i} = {n * i}")
+# tabuada(n=0)
+
+# QUESTÃO 5
+
+# def contador(n):
+#     n=10
+#     while n <=10:
+#         print(f"{n}")
+#         n -= 1
+#         if n<0:
+#             break
+#     print("Explosão!")
+# contador(n=0)
+
+# QUESTÃO 6
+
+# def con(n):
+#     soma_notas=0
+#     n=int(input("digite a quantidade de notas: "))
+#     for i in range(n):
+#         notas=float(input("digite a nota: "))
+#         soma_notas += notas
+#     print(soma_notas/n)
+# con(n=0) 
+
+# QUESTÃO 7
+
+# def fatorial(n):
+#     n = float(input("Digite um número: "))
+#     fatorial = 1
+#     i = 1
+#     while i <= n:
+#         fatorial *= i
+#         i += 1
+#     print(f"O fatorial de {n} é {fatorial}")
+# fatorial(n=0)
+    
+# QUESTÃO 8
+
+# def vogal(n):
+#     contador=0
+#     vogal = [ 'a' , 'A' , 'E' , 'e' , 'i' , 'I', 'O' , 'o' , 'u' , 'U']
+#     n=input("Digite uma palavra:  ")
+#     for v in vogal:
+#         if v in n :
+#            contador += 1  
+#     print(f"{n} tem {contador} vogais!")
+# vogal(n=0)
+
+# QUESTÃO 9
+
+
+
+
