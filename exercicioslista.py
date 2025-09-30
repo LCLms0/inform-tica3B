9d42e1e5b72a94344121edf48cf314a934c85c1a
antoinjose2010 <ajgorvenador22@gmail.com>
2025-09-30 06:31:00 -0300

    Quase tudo

---
+++ exercicioslista.py
@@ -0,0 +1,104 @@
+#1
+
+# nomes=[]
+# contador=0
+# for i in range(1,6):
+#     name=input(f"Digite um nome {i}: ")
+#     nomes.append(name)
+# print(nomes)
+
+# inicial=[letra[0] for letra in nomes ]
+# if i
+#     contador +=1
+# print(f"O total de iniciais é: {contador}")
+
+#2
+
+# notas=[]
+# for n in range(1,5):
+#     nota=float(input(f"Digite sua nota {n}: "))
+#     notas.append(nota)
+
+# def calcular_media(notas):
+#     media=sum(notas) / 4
+#     if media >= 7:
+#         print("Está aprovado!")
+#     else:
+#         print("Está reprovado!")
+
+# calcular_media(notas)
+
+#3
+
+# numeros=[]
+# for a in range(1,7):
+#     num=int(input(f"Digite o número {a}: "))
+#     numeros.append(num)
+
+# n=int(input("Digite um número para saber se está na lista: "))
+# if n in numeros:
+#     print(f"{n} está na lista!")
+# else:
+#     print(f"{n} não está na lista!")
+
+# posição=numeros.index(n)
+# print(f"O número {n} está na {posição} posição!")
+
+#4
+
+# pares=[]
+# for p in range(1,11):
+#     par=int(input(f"Digite o número {p}: "))
+#     pares.append(par)
+
+# def filtrar_pares(pares):
+#     for i in pares:
+#         if i % 2 == 0:
+#             print(f"Os pares são: {i}")
+
+# filtrar_pares(pares)
+
+#5
+
+# numer=[]
+# for r in range(1,9):
+#     nu=int(input(f"Digite o número {r}: "))
+#     numer.append(nu)
+
+# maior=max(numer)
+# menor=min(numer)
+# soma=sum(numer)
+# media=sum(numer) / r
+
+# print(F"Maior número é: {maior}")
+# print(F"Menor número é: {menor}")
+# print(F"Soma dos números é: {soma}")
+# print(F"Média dos números é: {media}")
+
+
+#6
+
+# numer=[]
+# for r in range(1,11):
+#     nu=int(input(f"Digite o número {r}: "))
+#     numer.append(nu)
+# v=int(input("Digite um número para remover da lista: "))
+
+# def remover_elemento(numer, v):
+#       nova_lista = [elemento for elemento in numer if elemento != v]
+#       print(f"{nova_lista}")
+
+# remover_elemento(numer, v)
+
+#7
+
+# convidados=[]
+# for c in range(1,6):
+#     con=input(f"Digite um nome {c}: ")
+#     convidados.append(con)
+
+# per=input("Qual nome deseja verificar na lista?: ")
+# if per in convidados:
+#     print("Convidado confirmado!")
+# else:
+#     print("Convidado não encontrado!")
