97a0721e9b8daf42c9a82017ae61dc66932e753f
antoinjose2010 <ajgorvenador22@gmail.com>
2025-11-16 09:42:33 -0300

    Tentando fazer

--- Sistema de Cardápio e Pedidos/funcoes.py
+++ Sistema de Cardápio e Pedidos/funcoes.py
@@ -1,19 +1,36 @@
-
+import tabulate
 
 def carregar_cardapio():
     print("------Canto do Palhano------")
 
     print("Salgados")
+    salgados = [ 
     {"id": 1, "nome": "Coxinha", "preco": (f"R$ 5,00)") },
     {"id": 2, "nome": "Pastel (carne/queijo/frango)", "preco": (f"R$ 7,00)") },
     {"id": 3, "nome": "Enroladinho de Salsicha", "preco": (f"R$ 4,00)") },
-    {"id": 4, "nome": "Empada (frango/palmito)", "preco": (f"R$ 6,00)") },
+    {"id": 4, "nome": "Empada (frango/palmito)", "preco": (f"R$ 6,00)") }, 
+    ]
 
     print("Lanches / Sanduíches")
+    lanches = [
     {"id": 5, "nome": "X-Salada", "preco": (f"R$ 14,00)") },
     {"id": 6, "nome": "X-Bacon", "preco": (f"R$ 18,00)") },
     {"id": 7, "nome": "Empada (frango/palmito)", "preco": (f"R$ 6,00)") },
     {"id": 8, "nome": "Empada (frango/palmito)", "preco": (f"R$ 6,00)") },
+    ]
+
+    print("Bebidas e Refrigerantes")
+    bebidas = [
+    {"id": 9, "nome": "Sucos", "preco": (f"R$ 5,00)") },
+    {"id": 10, "nome": "Coca-cola", "preco": (f"R$ 10,00)") },
+    {"id": 11, "nome": "Pepsi", "preco": (f"R$ 9,00)") },
+    ]
+
+def exibir_cardapio(cardapio):
+    return(carregar_cardapio)
+print(tabulate.tabulate(carregar_cardapio, headers='keys'))
+
+
 
 
 
--- Sistema de Cardápio e Pedidos/progama.py
+++ Sistema de Cardápio e Pedidos/progama.py
@@ -1,1 +1,26 @@
-print("------Canto do Palhano------")
+from funcoes import *
+
+def menu():
+    print("------Canto do Palhano------")
+
+    print("------Menu------")
+
+    print("[1] - Ver cardápio")
+    print("[2] - Adicionar item ao pedido")
+    print("[3] - Ver pedido")
+    print("[4] - Remover item")
+    print("[0] - Finalizar")
+
+    while True:
+
+        comando=int(input("Digite o comando: "))
+        if comando == 1:
+            exibir_cardapio()
+
+
+
+menu()
+
+
+
+
