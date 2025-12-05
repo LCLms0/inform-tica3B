e0af12a6ba8939ff3845e73d50dd181eaf0334ef
antoinjose2010 <ajgorvenador22@gmail.com>
2025-12-05 08:57:09 -0300

    Ainda fazendo

--- Sistema de Pedido de Lanches/main.py
+++ Sistema de Pedido de Lanches/main.py
@@ -11,6 +11,6 @@ cardapio = [
             {"id": 1, "nome": "Hambúrguer", "preco": 12.50}, 
             {"id": 2, "nome": "Batata Frita", "preco": 7.00}, 
             {"id": 3, "nome": "Refrigerante", "preco": 5.00} 
-    ]
+]
 
 id_itens=[]
--- Sistema de Pedido de Lanches/pedidos.py
+++ Sistema de Pedido de Lanches/pedidos.py
@@ -2,12 +2,18 @@ from main import *
 
 
 def novo_pedido(cardapio):
-    pedido=[]
-    quant_itens=int()
+    pedido={
+        "ID": ,
+        "Quantidade":,
+        }
     itens=int(input("Digite o id do item para adicionar ao pedido: "))
+    quant_itens=int(input("Digite a quantidade do item: "))
     if itens in cardapio["id"]:
-        pedido.append(itens)
+        pedido["ID"] = itens
         print("Item adicionado com sucesso!")
+    else:
+        print("Id não encontrado!")
+    pedido["Quantidade"] = quant_itens
     
 
 
